66ed416842dc99a1104d17bf197d45df806f9a4b
blackmos233 <96555737+mcxiaoyan233@users.noreply.github.com>
2023-06-21 00:39:56 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.zhihu.com'
+      url.hostname = 'www.ip138.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
